106e0f50bb952f8681493fc5cdcb9ae9134ea798
Sunset-kim <wood2002@naver.com>
2022-08-25 00:11:58 +0900

    BinaryHeap - refactoring

--- BinaryHeap/index.js
+++ BinaryHeap/index.js
@@ -7,20 +7,23 @@ class BinaryHeap {
     if (this.array.length === 0) {
       return this.array.push(value);
     }
-
     this.array.push(value);
-    let currentIndex = this.array.length - 1;
-    while (true) {
-      const parentIndex = Math.floor((currentIndex - 1) / 2);
 
-      if (this.array[parentIndex] < this.array[currentIndex]) {
-        const temp = this.array[parentIndex];
-        this.array[parentIndex] = this.array[currentIndex];
-        this.array[currentIndex] = temp;
-        currentIndex = parentIndex;
-      } else {
-        return;
+    let currentIndex = this.array.length - 1;
+    const element = this.array[currentIndex];
+
+    while (currentIndex > 0) {
+      const parentIndex = Math.floor((currentIndex - 1) / 2);
+      const parent = this.array[parentIndex];
+
+      if (element <= parent) {
+        break;
       }
+
+      const temp = this.array[parentIndex];
+      this.array[parentIndex] = element;
+      this.array[currentIndex] = parent;
+      currentIndex = parentIndex;
     }
   }
 }
@@ -33,6 +36,6 @@ binaryHeap.insert(66);
 binaryHeap.insert(41);
 binaryHeap.insert(44);
 binaryHeap.insert(16);
-binaryHeap.insert(97);
+// binaryHeap.insert(97);
 
 console.log(binaryHeap);
