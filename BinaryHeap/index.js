0e33a1c9d00d888bb46b97bfa563431416fa46a9
Sunset-kim <wood2002@naver.com>
2022-08-25 00:38:02 +0900

    Binary Heap - extract Max

--- BinaryHeap/index.js
+++ BinaryHeap/index.js
@@ -19,14 +19,59 @@ class BinaryHeap {
       if (element <= parent) {
         break;
       }
-
-      const temp = this.array[parentIndex];
       this.array[parentIndex] = element;
       this.array[currentIndex] = parent;
       currentIndex = parentIndex;
     }
   }
+
+  extractMax() {
+    if (this.array.length === 0) return;
+    const max = this.array[0];
+    const end = this.array.pop();
+    this.array[0] = end;
+    this.bubbleDown();
+
+    return max;
+  }
+
+  bubbleDown() {
+    let index = 0;
+
+    const length = this.array.length;
+    const element = this.array[0];
+
+    while (true) {
+      let leftChildIdx = 2 * index + 1;
+      let rightChildIdx = 2 * index + 2;
+
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.array[leftChildIdx];
+
+        if (leftChild > element) {
+          swap = leftChildIdx;
+        }
+      }
+
+      if (rightChildIdx < length) {
+        rightChild = this.array[rightChildIdx];
+        if ((swap === null && rightChild > element) || (swap !== null && rightChild > leftChild)) {
+          swap = rightChildIdx;
+        }
+      }
+
+      if (swap === null) break;
+      this.array[index] = this.array[swap];
+      this.array[swap] = element;
+      index = swap;
+    }
+  }
 }
+
+// 33 39 33 18 27 12
 
 const binaryHeap = new BinaryHeap();
 
@@ -36,6 +81,12 @@ binaryHeap.insert(66);
 binaryHeap.insert(41);
 binaryHeap.insert(44);
 binaryHeap.insert(16);
-// binaryHeap.insert(97);
+binaryHeap.insert(97);
 
+console.log(binaryHeap);
+
+console.log(binaryHeap.extractMax());
+console.log(binaryHeap.extractMax());
+console.log(binaryHeap.extractMax());
+console.log(binaryHeap.extractMax());
 console.log(binaryHeap);
