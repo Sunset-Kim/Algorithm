2408dd32a978e4c1ae7ee92038d6de368d4482ab
Sunset-kim <wood2002@naver.com>
2022-08-27 01:36:07 +0900

    우선순위큐 - enqueue

--- PriorityQueue/PriorityQueue.js
+++ PriorityQueue/PriorityQueue.js
@@ -12,8 +12,36 @@ class PriorityQueue {
 
   enqueue(priority, value) {
     const newNode = new Node(priority, value);
-    if (this.queue.length === 0) return this.queue.push(newNode);
+    this.queue.push(newNode);
 
-    while (true) {}
+    let currentIndex = this.queue.length - 1;
+
+    while (currentIndex > 0) {
+      const parentIndex = parseInt((currentIndex - 1) / 2);
+      const parentNode = this.queue[parentIndex];
+      const currentNode = this.queue[currentIndex];
+      let swap = false;
+
+      if (parentNode.priority > currentNode.priority) {
+        this.queue[parentIndex] = currentNode;
+        this.queue[currentIndex] = parentNode;
+        currentIndex = parentIndex;
+        swap = true;
+      }
+
+      if (!swap) return this.quque;
+    }
+
+    return this.queue;
   }
 }
+
+const priorityQueue = new PriorityQueue();
+
+console.log(priorityQueue.enqueue(10, 1));
+console.log(priorityQueue.enqueue(8, 1));
+console.log(priorityQueue.enqueue(6, 1));
+console.log(priorityQueue.enqueue(7, 1));
+console.log(priorityQueue.enqueue(5, 1));
+console.log(priorityQueue.enqueue(3, 1));
+console.log(priorityQueue.enqueue(1, 1));
