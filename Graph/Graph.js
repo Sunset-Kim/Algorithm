54ee4b07467441097092d520c7f4d6a6c7cdcf68
Sunset-kim <wood2002@naver.com>
2022-08-30 23:59:08 +0900

    graph - method add edge

--- Graph/Graph.js
+++ Graph/Graph.js
@@ -7,6 +7,18 @@ class Graph {
     if (this.adjacencyList[key]) return;
     this.adjacencyList[key] = [];
   }
+
+  /**
+   * 2. adding an edge
+   * - 두개의 vertex를 받음
+   * - vertex1을 list에서 찾아서 vertex2의 키를 push
+   * - vertex2을 list에서 찾아서 vertex1의 키를 push
+   */
+
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
 }
 
 const g = new Graph();
